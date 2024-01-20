250126c63776be6a36b84484382f054a915e5024
Emens Fede <emens.fede@gmail.com>
2024-01-19 23:38:46 -0300

    feat: add icons and google fonts

--- src/shared/components/sidebar/sidebar.tsx
+++ src/shared/components/sidebar/sidebar.tsx
@@ -2,24 +2,51 @@ import { Link } from "react-router-dom";
 import "./sidebar.scss";
 
 function Sidebar() {
+  let routes = [
+    {
+      link: "/home",
+      title: "Inicio",
+      icon: "home",
+    },
+    {
+      link: "/goals",
+      title: "Objetivos",
+      icon: "description",
+    },
+    {
+      link: "/tasks",
+      title: "Tareas",
+      icon: "task",
+    },
+    {
+      link: "/home",
+      title: "Estadisticas",
+      icon: "analytics",
+    },
+  ];
+
   return (
     <div className="sidebar">
-      <div className="profile">Perfil</div>
-      <div className="pages">
-        <Link to="/home" className="link">
-          Inicio
-        </Link>
-        <Link to="/goals" className="link">
-          Objetivos
-        </Link>
-        <Link to="/tasks" className="link">
-          Tareas
-        </Link>
-        <Link to="/goals" className="link">
-          Estadisticas
-        </Link>
+      <div className="profile sidebar-element">
+        <span className="material-icons-round">account_circle</span>
+        Perfil
       </div>
-      <div className="config">Configuracion</div>
+
+      <div className="pages">
+        {routes.map((route, i) => {
+          return (
+            <Link to={route.link} key={i} className="link sidebar-element">
+              <span className="material-icons-round">{route.icon}</span>
+              {route.title}
+            </Link>
+          );
+        })}
+      </div>
+
+      <div className="config sidebar-element">
+        <span className="material-icons-round">settings</span>
+        Configuracion
+      </div>
     </div>
   );
 }
